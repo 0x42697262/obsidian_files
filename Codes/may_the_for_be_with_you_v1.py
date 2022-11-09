afedbcc92d28132bf67c1ff1da74f42fd22ddb23
KrulYuno <giogulane.wineffgulane@gmail.com>
2022-11-09 18:12:06 +0900

    update README.md soon

---
+++ Codes/may_the_for_be_with_you_v1.py
@@ -0,0 +1,68 @@
+"""
+Reference:
+    https://github.com/huzaifamaw/Lexical_Analyzer-Parser_Implemented-in-Python/blob/master/main.py
+"""
+
+import re
+
+TOKEN_DEFINITION = {
+        '='  : 'ASSIGN',
+        '<'  : 'LESS_THAN',
+        '>'  : 'GREATER_THAN',
+        '++' : 'SELF_PLUS',
+        '--' : 'SELF_MINUS',
+        '+'  : 'PLUS_OPERATOR',
+        '-'  : 'MINUS_OPERATOR',
+        '*'  : 'MULTIPLY_OPERATOR',
+        '/'  : 'DIVIDE_OPERATOR',
+        '<=' : 'LESS_THAN_EQUAL',
+        '>=' : 'GREATER_THAN_EQUAL',
+        '+=' : 'PLUS_EQUAL',
+        '-=' : 'MINUS_EQUAL',
+        '*=' : 'MULTIPLY_EQUAL',
+        '/=' : 'DIVIDE_EQUAL',
+        '('  : 'OPEN_PARENTHESIS',
+        ')'  : 'CLOSE_PARENTHESIS',
+        '['  : 'OPEN_BRACKET',
+        ']'  : 'CLOSE_BRACKET',
+        '{'  : 'OPEN_BRACE',
+        '}'  : 'CLOSE_BRACE',
+        ','  : 'COMMA',
+        '"'  : 'DOUBLE_QUOTE',
+        '\'' : 'SINGLE_QUOTE',
+        ';'  : 'SEMICOLON',
+        '&&' : 'AND',
+        '||' : 'OR',
+        '<<' : 'LEFT_BITWISE',
+        '>>' : 'RIGHT_BITWISE',
+        '==' : 'IF_EQUAL',
+        }
+
+TOKEN_KEYWORD = {
+        'strict'     :  ["if", "else", "while", "for", "cout", "cin", "return"],
+        'brackets'   :  ['(', ')', '{', '}', '[', ']'],
+        'data_types' :  ["int", "float", "char", "string", "bool", "void"],
+        'arithmetic' :  ['+', '-', '/', '*'],
+        'logical'    :  ['||', '&&'],
+        } 
+
+class Lexer:
+    def __init__(self, expression):
+        self.expression = expression
+
+    
+
+
+def main():
+    # Take inputs, assume inputs are correct
+    # no sanitization
+    lines = int(input())
+    expression = str()
+    for _ in range(lines):
+        expression += input()
+
+    print(expression)
+
+
+if __name__ == "__main__":
+    main()
