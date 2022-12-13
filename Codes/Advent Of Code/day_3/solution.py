af6e50bf1657baf5d906ae70609efd3036bae71a
KrulYuno <giogulane.wineffgulane@gmail.com>
2022-12-13 10:54:03 +0900

    AoC: Day 3 (Done)

--- Codes/Advent Of Code/day_3/solution.py
+++ Codes/Advent Of Code/day_3/solution.py
@@ -23,4 +23,32 @@ def part1():
                 priority_sum    = priority_sum + priority_list[c]
     print(priority_sum)
 
+def part2():
+    item_found      = dict()
+    priority_sum    = 0
+    for _ in range(97,123):
+        item_found[chr(_)]   = [False, False, False]
+    for _ in range(65, 91):
+        item_found[chr(_)]   = [False, False, False]
+
+    line    = 0
+    for rucksacks in puzzle_input:
+        line = line + 1
+        for item in rucksacks.strip():
+            item_found[item][(line - 1 ) % 3]   = True
+
+        if line % 3 == 0:
+            for _ in range(97,123):
+                if False not in item_found[chr(_)]:
+                    priority_sum    = priority_sum + priority_list[chr(_)]
+                item_found[chr(_)]   = [False, False, False]
+            for _ in range(65, 91):
+                if False not in item_found[chr(_)]:
+                    priority_sum    = priority_sum + priority_list[chr(_)]
+                item_found[chr(_)]   = [False, False, False]
+    
+    print(priority_sum)
+
+
 part1()
+part2()
