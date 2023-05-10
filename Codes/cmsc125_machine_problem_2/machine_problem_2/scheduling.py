4ede29d2def0ba0482e78b717128d91fc3ee2607
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-05-11 07:26:06 +0900

    [020 Studies] CMSC125 MP2 -- SRPT done, lacking roundrobing and gantchart

--- Codes/cmsc125_machine_problem_2/machine_problem_2/scheduling.py
+++ Codes/cmsc125_machine_problem_2/machine_problem_2/scheduling.py
@@ -3,7 +3,8 @@ class SchedulingAlgorithm:
         self.process = {}
         self.highest_job    = -1
         self.lowest_job     = -1
-        self.data    = []
+        self.data           = []
+        self.gant_chart     = []
 
         self.total_waiting_time     = 0
         self.total_turnaround_time  = 0
@@ -57,6 +58,9 @@ class SchedulingAlgorithm:
     def calculate_avg_computing_time(self):
         return self.total_computing_time / len(self.data)
 
+    def append_job_to_gant_chart(self, job, end_time):
+        self.gant_chart.append({job : end_time})
+
     def show_stats(self):
         for p in self.data:
             print(f"job: {p[0]} --> {p[1]}")
@@ -70,27 +74,6 @@ class FCFSAlgo(SchedulingAlgorithm):
 
         self.data = sorted(list(self.process.items()), key=lambda x: x[1]['arrival_time'])
 
-    def calculate_completion_time(self, index):
-        return super().calculate_completion_time(index)
-
-    def calculate_waiting_time(self, index):
-        return super().calculate_waiting_time(index)
-
-    def calculate_turnaround_time(self, index):
-        return super().calculate_turnaround_time(index)
-
-    def calculate_computing_time(self, index):
-        return super().calculate_computing_time(index)
-
-    def calculate_avg_waiting_time(self):
-        return super().calculate_avg_waiting_time()
-
-    def calculate_avg_turnaround_time(self):
-        return super().calculate_avg_turnaround_time()
-
-    def calculate_avg_computing_time(self):
-        return super().calculate_avg_computing_time()
-
         
 
 
@@ -103,39 +86,84 @@ class SJFAlgo(SchedulingAlgorithm):
         self.data = sorted(list(self.process.items()), key=lambda x: x[1]['burst_time'])
 
     
-    def calculate_completion_time(self, index):
-        return super().calculate_completion_time(index)
-
-    def calculate_waiting_time(self, index):
-        return super().calculate_waiting_time(index)
-
-    def calculate_turnaround_time(self, index):
-        return super().calculate_turnaround_time(index)
-
-    def calculate_computing_time(self, index):
-        return super().calculate_computing_time(index)
-
-    def calculate_avg_waiting_time(self):
-        return super().calculate_avg_waiting_time()
-
-    def calculate_avg_turnaround_time(self):
-        return super().calculate_avg_turnaround_time()
-
-    def calculate_avg_computing_time(self):
-        return super().calculate_avg_computing_time()
-
 
 
 class SRPTAlgo(SchedulingAlgorithm):
     def __init__(self, process):
         super().__init__(process)
 
-        self.original_data  = process
-        self.data           = []
-        self.initial_data   = sorted(list(self.process.items()), key=lambda x: x[1]['arrival_time'])
+        self.data           = self.process
+        for key, value in self.data.items():
+            self.data[key]['cpu_remaining_time'] = value['burst_time']
 
-        self.total_time     = 0
+        self.total_time = 0
+        self.scheduled = None
+        self.queue = []
+        self.completed = 0
 
+        while self.completed != len(self.data):
+            if not self.scheduled:
+                for j, v in self.data.items():
+                    if self.total_time == v['arrival_time']:
+                        self.scheduled = j
+                        self.total_time = v['arrival_time']
+
+                        break
+            else:
+                if self.data[self.scheduled]['cpu_remaining_time'] == 0:
+                    self.data[self.scheduled]['completion_time'] = self.total_time
+                    self.completed += 1
+                    self.scheduled = None
+
+                    # check new arrival
+                    for j, v in self.data.items():
+                        if self.total_time == v['arrival_time']:
+                            self.scheduled = j
+                            break
+
+                    # check if new self.scheduled is lower than in the self.queue
+                    if self.scheduled and self.queue:
+                        self.queue.append(self.scheduled)
+                        self.scheduled = None
+
+                    if not self.scheduled and self.queue:
+                        self.queue.sort()
+                        value = float('inf')
+                        for j in self.queue:
+                            if self.data[j]['cpu_remaining_time'] < value:
+                                value = self.data[j]['cpu_remaining_time']
+                                self.scheduled = j
+                        self.queue.remove(self.scheduled)
+                    
+                else:
+                    # check new arrival
+                    for j, v in self.data.items():
+                        if self.total_time == v['arrival_time']:
+                            if self.data[self.scheduled]['cpu_remaining_time'] < v['cpu_remaining_time']:
+                                self.queue.append(j)
+                            elif self.data[self.scheduled]['cpu_remaining_time'] == v['cpu_remaining_time']:
+                                if self.data[self.scheduled]['arrival_time'] < v['arrival_time']:
+                                    self.queue.append(j)
+                                else:
+                                    self.queue.append(self.scheduled)
+                                    self.scheduled = j
+                            else:
+                                self.queue.append(self.scheduled)
+                                self.scheduled = j
+                            break
+            
+            if self.scheduled:
+                self.total_time += 1
+                self.data[self.scheduled]['cpu_remaining_time'] -= 1
+
+        self.data           = sorted(list(self.data.items()), key=lambda x: x[1]['completion_time'])
+
+    def calculate_completion_time(self, index):
+        pass
+
+    def calculate_turnaround_time(self, index):
+        self.data[index][1]['turnaround_time'] = self.data[index][1]['completion_time'] - self.data[index][1]['arrival_time']
+        self.total_turnaround_time          += self.data[index][1]['turnaround_time']
 
 
 
@@ -145,27 +173,6 @@ class PriorityAlgo(SchedulingAlgorithm):
         super().__init__(process)
 
         self.data = sorted(list(self.process.items()), key=lambda x: x[1]['priority'])
-
-    def calculate_completion_time(self, index):
-        return super().calculate_completion_time(index)
-
-    def calculate_waiting_time(self, index):
-        return super().calculate_waiting_time(index)
-
-    def calculate_turnaround_time(self, index):
-        return super().calculate_turnaround_time(index)
-
-    def calculate_computing_time(self, index):
-        return super().calculate_computing_time(index)
-
-    def calculate_avg_waiting_time(self):
-        return super().calculate_avg_waiting_time()
-
-    def calculate_avg_turnaround_time(self):
-        return super().calculate_avg_turnaround_time()
-
-    def calculate_avg_computing_time(self):
-        return super().calculate_avg_computing_time()
 
 
 
